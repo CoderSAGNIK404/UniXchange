81408403d8cc6375620d29365c88d5e15283c21e
Sagnik Sarkar <sagniksarkar2025@gmail.com>
2026-01-01 01:27:06 +0530

    Prepare for deployment: backend serving frontend

--- server/index.js
+++ server/index.js
@@ -51,8 +51,12 @@ app.use('/api/orders', orderRoutes);
 app.use('/api/users', userRoutes);
 
 
-app.get('/', (req, res) => {
-    res.send('UniXchange API is running');
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// Anything that doesn't match the above routes, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(PORT, () => {
